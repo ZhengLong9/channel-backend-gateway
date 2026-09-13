49d5dceca12e13898ed1a0dc92081dd9c6c31113
ZhengLong9 <soozhenglong0309@gmail.com>
2026-09-13 12:35:24 +0800

    Initial Spring Boot Scaffold with CI workflow

---
+++ src/main/java/com/zheng/middleware/channel_backend_gateway/ChannelBackendGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.middleware.channel_backend_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelBackendGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelBackendGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zheng/middleware/channel_backend_gateway/ChannelBackendGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zheng.middleware.channel_backend_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannelBackendGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
